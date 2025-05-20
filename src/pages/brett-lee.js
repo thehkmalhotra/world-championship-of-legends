832a5299be1068c23fe6d520007b05420749e493
Pradyum Khandelwal <pradyumkhandelwal2002@gmail.com>
2025-05-20 20:43:21 +0530

    Update brett-lee.js
    comment style import

--- src/pages/brett-lee.js
+++ src/pages/brett-lee.js
@@ -1,7 +1,7 @@
 // pages/brett-lee.js
 import Head from "next/head";
 import Layout from "@/components/Layout";
-import styles from "@/styles/Player.module.css";
+// import styles from "@/styles/Player.module.css";
 
 export default function BrettLeePage() {
   return (
